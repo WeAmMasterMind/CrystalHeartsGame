453d10944f13cb334d764af9a286d673895ada3c
Wiam Gh <126759826+WeAmMasterMind@users.noreply.github.com>
2025-05-20 12:48:56 +0300

    source code upload

---
+++ source/main.js
@@ -0,0 +1,23 @@
+/*
+
+University of London
+BsC Computer Science
+Agile Software Project 
+Crystal Hearts Game
+
+Davide Basile
+Wiam Ghousaini 
+Nelson Waissman
+
+Main File: main.js
+Main program file is used to define the game objects and run the game loop
+
+*/
+
+function main() {
+    const game = new Game();
+    game.startGame();
+}
+
+// Run the main function to start the game
+main();
